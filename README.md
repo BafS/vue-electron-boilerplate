da57e55242537f3683d0976f1a733b5fe5925525
BafS <fabacrans@hotmail.com>
2016-04-17 22:55:16 +0200

    Init.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vue Electron Boilerplate (alpha)
+
+> A Vue, Vuex, ES6, Electron Boilerplate with Hot loading based on [vue-webpack-boilerplate](https://github.com/vuejs-templates/webpack)
+
+## Dev Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload
+# and start electron
+# (run *both*)
+npm run dev:server
+npm run dev:electron
+```
+
+## Build Setup
+
+```
+# build for production with minification
+npm run build
+```
+
+- To package your application check [electron-packager](https://github.com/electron-userland/electron-packager).
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <hello></hello>
+    <p>
+      Welcome to your <strong>Vue Electron Boilerplate</strong> app!
+    </p>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import store from './vuex/store'
+import Hello from './components/Hello'
+import Counter from './components/Counter'
+
+export default {
+  store,
+  components: {
+    Hello,
+    Counter
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
